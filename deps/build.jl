2371ee13736851600cdf0ef9200d8091f9b83398
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2023-08-22 19:27:14 +0200

    New

--- deps/build.jl
+++ deps/build.jl
@@ -1,7 +1,9 @@
 using Libdl
 
-"""
-run(`tar xfz $(@__DIR__)/$nautyver.tar.gz -C $(@__DIR__)`)
-cd(()->run(`./configure --enable-tls`), nautydir)
-cd(()->run(`make nauty.a CCOBJ='${CC} -fPIC -c ${CFLAGS} -o $@'`), nautydir)
-"""
+cd("spasm") do
+    run(`autoreconf -i`)
+    run(`./configure --enable-openmp`)
+    cd("src") do
+        run(`make`)
+    end
+end
--- src/Spasm.jl
+++ src/Spasm.jl
@@ -1,11 +1,11 @@
 module Spasm
 
-using SparseArrays
+using SparseArrays #, Libdl
 
 import Base: unsafe_convert
 import SparseArrays: nnz
 
-const spasm_lib = const LIB_FILE = "$(@__DIR__)" * "/../deps/spasm/src/.libs/libspasm.dylib"
+const spasm_lib = const LIB_FILE = "$(@__DIR__)" * "/../deps/spasm/src/.libs/libspasm." * "dylib" #Libdl.dlext
 
 struct GFp v::Cuint end
 GFp(x::Integer,prime = 42013) = GFp(Cuint(mod(x,prime)))
@@ -39,7 +39,7 @@ mutable struct spasm
             unsafe_store!(spasmA.j,A.rowval[i]-1,i)
             unsafe_store!(spasmA.x,mod(A.nzval[i],prime),i)
         end
-        finalizer(csr_free,spasmA)
+        finalizer(csr_free1,spasmA)
         spasmA
     end
 end
@@ -53,44 +53,50 @@ function SparseArrays.sparse(A::spasm)
     SparseMatrixCSC{GFp,Int}(A.m,A.n,[Int(unsafe_load(A.p,i))+1 for i=1:A.n+1],[Int(unsafe_load(A.j,i))+1 for i=1:nnz],[unsafe_load(A.x,i) for i=1:nnz])
 end
 
-unsafe_load1(x) = unsafe_load(x,1)
+spasm(x::Ptr{spasm}) = (A = unsafe_load(x,1); finalizer(csr_free,A); A)
+
+permutation(x) = (x == nothing ? C_NULL : isa(Vector{Cint},x) ? pointer(x) : error("$x should be nothing or a permutation{Cint}"))
 
 # from spasm_util.c
 
 nnz(A::spasm) = Int(@ccall spasm_lib."spasm_nnz"(pointer_from_objref(A)::Ptr{spasm})::Cint)
 
-csr_alloc(m,n,nzmax,prime,with_values) = unsafe_load1(@ccall spasm_lib."spasm_csr_alloc"(m::Cint,n::Cint,nzmax::Cint,prime::Cint,with_values::Cint)::Ptr{spasm})
+csr_alloc(m,n,nzmax,prime,with_values) = spasm(@ccall spasm_lib."spasm_csr_alloc"(m::Cint,n::Cint,nzmax::Cint,prime::Cint,with_values::Cint)::Ptr{spasm})
 
 csr_realloc(A::spasm,nzmax) = @ccall spasm_lib."spasm_csr_realloc"(pointer_from_objref(A)::Ptr{spasm},nzmax::Cint)::Cvoid
 
 csr_resize(A::spasm,m,n) = @ccall spasm_lib."spasm_csr_resize"(pointer_from_objref(A)::Ptr{spasm},m::Cint,n::Cint)::Cvoid
 
-# cannot directly call csr_free because the spasm structure itself belongs to Julia
-csr_free(A::spasm) = (@ccall free(A.p::Ptr{Cvoid})::Cvoid; @ccall free(A.j::Ptr{Cvoid})::Cvoid; @ccall free(A.x::Ptr{Cvoid})::Cvoid)
+csr_free(A::spasm) = @ccall spasm_lib."spasm_csr_free"(pointer_from_objref(A)::Ptr{spasm})::Cvoid
+
+# sometimes, we should not directly call csr_free because the spasm structure itself belongs to Julia
+csr_free1(A::spasm) = (@ccall free(A.p::Ptr{Cvoid})::Cvoid; @ccall free(A.j::Ptr{Cvoid})::Cvoid; @ccall free(A.x::Ptr{Cvoid})::Cvoid)
 
 # declared, not defined
-# identity(n,prime) = (A = unsafe_load1(@ccall spasm_lib."spasm_identity"(n::Cint,prime::Cint)::Ptr{spasm}); finalizer(csr_free,A); A)
+# identity(n,prime) = sparsm(@ccall spasm_lib."spasm_identity"(n::Cint,prime::Cint)::Ptr{spasm})
 
 get_num_threads() = Int(@ccall spasm_lib."spasm_get_num_threads"()::Cint)
 get_thread_num() = Int(@ccall spasm_lib."spasm_get_thread_num"()::Cint)
 
 # from spasm_transpose.c
 
-Base.transpose(A::spasm) = unsafe_load1(@ccall spasm_lib."spasm_transpose"(pointer_from_objref(A)::Ptr{spasm})::Ptr{spasm})
+Base.transpose(A::spasm) = spasm(@ccall spasm_lib."spasm_transpose"(pointer_from_objref(A)::Ptr{spasm})::Ptr{spasm})
 
 # from spasm_submatrix.c
 
-submatrix(A::spasm,r::UnitRange{Int},c::UnitRange{Int},with_values) = unsafe_load1(@ccall spasm_lib."spasm_submatrix"(pointer_from_objref(A)::Ptr{spasm},r.start::Cint,r.stop::Cint,c.start::Cint,c.stop::Cint,with_value::Cint)::Ptr{spasm})
+submatrix(A::spasm,r::UnitRange{Int},c::UnitRange{Int},with_values) = spasm(@ccall spasm_lib."spasm_submatrix"(pointer_from_objref(A)::Ptr{spasm},r.start::Cint,r.stop::Cint,c.start::Cint,c.stop::Cint,with_value::Cint)::Ptr{spasm})
 
-sorted_submatrix(A::spasm,r::UnitRange{Int},c::UnitRange{Int},with_values) = unsafe_load1(@ccall spasm_lib."sorted_spasm_submatrix"(pointer_from_objref(A)::Ptr{spasm},r.start::Cint,r.stop::Cint,c.start::Cint,c.stop::Cint,with_value::Cint)::Ptr{spasm})
+sorted_submatrix(A::spasm,r::UnitRange{Int},c::UnitRange{Int},with_values) = spasm(@ccall spasm_lib."sorted_spasm_submatrix"(pointer_from_objref(A)::Ptr{spasm},r.start::Cint,r.stop::Cint,c.start::Cint,c.stop::Cint,with_value::Cint)::Ptr{spasm})
 
-rows_submatrix(A::spasm,r::UnitRange{Int},with_values) = unsafe_load1(@ccall spasm_lib."spasm_rows_submatrix"(pointer_from_objref(A)::Ptr{spasm},r.start::Cint,r.stop::Cint,with_value::Cint)::Ptr{spasm})
+rows_submatrix(A::spasm,r::UnitRange{Int},with_values) = spasm(@ccall spasm_lib."spasm_rows_submatrix"(pointer_from_objref(A)::Ptr{spasm},r.start::Cint,r.stop::Cint,with_value::Cint)::Ptr{spasm})
+
+# from spasm_permutation.c
 
 """
-# from spasm_permutation.c
-void spasm_pvec(const int *p, const spasm_GFp * b, spasm_GFp * x, int n);
+# pvec(const int *p, const spasm_GFp * b, spasm_GFp * x, int n);
 void spasm_ipvec(const int *p, const spasm_GFp * b, spasm_GFp * x, int n);
-int *spasm_pinv(int const *p, int n);
+
+# pinv(p::Vector{Cint}) = int const *p, int n); # requires freeing
 spasm *spasm_permute(const spasm * A, const int *p, const int *qinv, int with_values);
 int *spasm_random_permutation(int n);
 void spasm_range_pvec(int *x, int a, int b, int *p);
@@ -112,15 +118,28 @@ void spasm_gaxpy(const spasm * A, const spasm_GFp * x, spasm_GFp * y);
 int spasm_sparse_vector_matrix_prod(const spasm * M, const spasm_GFp * x, const int *xi, int xnz, spasm_GFp * y, int *yi)
 ;
 
-/* spasm_triangular.c */
-int spasm_is_upper_triangular(const spasm * A);
-int spasm_is_lower_triangular(const spasm * A);
-void spasm_dense_back_solve(const spasm * L, spasm_GFp * b, spasm_GFp * x, const int *p);
-int spasm_dense_forward_solve(const spasm * U, spasm_GFp * b, spasm_GFp * x, const int *q);
-int spasm_sparse_backward_solve(const spasm * L, const spasm * B, int k, int *xi, spasm_GFp * x, const int *pinv, int r_b
-ound);
-int spasm_sparse_forward_solve(const spasm * U, const spasm * B, int k, int *xi, spasm_GFp * x, const int *pinv);
+"""
 
+# from spasm_triangular.c
+
+is_upper_triangular(A::spasm) = Bool(@ccall spasm_lib."spasm_is_upper_triangular"(pointer_from_objref(A)::Ptr{spasm})::Cint)
+
+is_lower_triangular(A::spasm) = Bool(@ccall spasm_lib."spasm_is_lower_triangular"(pointer_from_objref(A)::Ptr{spasm})::Cint)
+
+# solve x . L = b where x and b are dense. Trashes b.
+# p[j] == i indicates if the "diagonal" entry on column j is on row i.
+dense_back_solve(L::spasm, b::Vector{GFp}, p = nothing, x::Vector{GFp} = Vector{GFp}(undef,L.n)) = (@ccall spasm_lib."spasm_dense_back_solve"(pointer_from_objref(L)::Ptr{spasm}, pointer(b)::Ptr{Cint}, pointer(x)::Ptr{GFp}, permutation(p)::Ptr{Cint})::Cvoid; x)
+
+# solve x . U = b where x and b are dense. Trashes b.
+# q[i] indicates the column on which the i-th row pivot is.
+dense_forward_solve(U::spasm, b::Vector{GFp}, q = nothing, x::Vector{GFp} = Vector{GFp}(undef,L.n)) = (@ccall spasm_lib."spasm_dense_forward_solve"(pointer_from_objref(U)::Ptr{spasm}, pointer(b)::Ptr{Cint}, pointer(x)::Ptr{GFp}, permutation(q)::Ptr{Cint})::Cvoid; x)
+
+sparse_backward_solve(L::spasm, B::spasm, k, pinv, r_bound, xi::Vector{Cint} = zeros(Cint,L.m), x::Vector{GFp} = Vector{GFp}(undef,L.m)) = (top = @ccall spasm_lib."spasm_sparse_backward_solve"(pointer_from_objref(L)::Ptr{spasm}, pointer_from_objref(B)::Ptr{Cint}, k::Cint, pointer(xi)::Ptr{Cint}, pointer(x)::Ptr{GFp}, permutation(pinv)::Ptr{Cint}, r_bound::Cint)::Cint; (view(xi,top:L.m),x))
+
+# solve x * U = B[k], where U is (permuted) upper triangular.
+sparse_forward_solve(U::spasm, B::spasm, k, pinv = nothing, xi::Vector{Cint} = zeros(Cint,U.m), x::Vector{GFp} = Vector{GFp}(undef,U.m)) = (top = @ccall spasm_lib."spasm_sparse_forward_solve"(pointer_from_objref(U)::Ptr{spasm}, pointer_from_objref(B)::Ptr{Cint}, k::Cint, pointer(xi)::Ptr{Cint}, pointer(x)::Ptr{GFp}, permutation(pinv)::Ptr{Cint})::Cint; (view(xi,top:U.m),x))
+
+"""
 /* spasm_lu.c */
 spasm_lu *spasm_PLUQ(const spasm * A, const int *row_permutation, int keep_L);
 spasm_lu *spasm_LU(const spasm * A, const int *row_permutation, int keep_L);
@@ -167,7 +186,7 @@ spasm_dm *spasm_connected_components(const spasm * A, spasm * given_At);
 """
 
 # from spasm_kernel.c
-kernel(A,column_permutation = nothing) = unsafe_load1(@ccall spasm_lib."spasm_kernel"(pointer_from_objref(A)::Ptr{spasm},(column_permutation == nothing ? C_NULL : pointer(column_permutation))::Ptr{Cint})::Ptr{spasm})
+kernel(A,column_permutation = nothing) = spasm(@ccall spasm_lib."spasm_kernel"(pointer_from_objref(A)::Ptr{spasm},permutation(column_permutation)::Ptr{Cint})::Ptr{spasm})
 
 """
 /* spasm_uetree.c */
