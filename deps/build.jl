0a7b46037177ac092a2c8de194fd80e35616fcb7
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2023-12-22 11:53:32 +0100

    fix build

--- deps/build.jl
+++ deps/build.jl
@@ -4,12 +4,12 @@ curdir = pwd()
 
 cd("givaro") do
     run(`./autogen.sh`)
-    run(`./make install prefix=$curdir/usr`)
+    run(`make install prefix=$curdir/usr`)
 end
 
 cd("fflas-ffpack") do
     run(`./autogen.sh GIVARO_CFLAGS=-I../usr/include`)
-    run(`./make install prefix=$curdir/usr`)
+    run(`make install prefix=$curdir/usr`)
 end
 
 cd("spasm") do
