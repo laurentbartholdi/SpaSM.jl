759c97d1349c25a982326860a8283910c780a592
Laurent Bartholdi <laurent.bartholdi@gmail.com>
2024-11-05 13:58:20 +0100

    Forgot to rename Spasm -> SpaSM in tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,12 @@
-using Spasm, SparseArrays, LinearAlgebra, Test
+using SpaSM, SparseArrays, LinearAlgebra, Test
 
 m = sparse([1,1,3,3],[1,2,3,4],[1,2,3,4])
 
-F = Spasm.Field(42013)
+F = SpaSM.Field(42013)
 
 @testset "Construction of spasm matrix" begin
     sm = CSR(m)
-    @test sparse(sm) == Spasm.ZZp{F}.(m)
+    @test sparse(sm) == SpaSM.ZZp{F}.(m)
 end
 
 @testset "Transpose" begin
@@ -18,7 +18,7 @@ end
     sm = CSR(m)
     
     k = kernel(sm)
-    @test sparse(k) == sparse([2],[1],Spasm.ZZp{F}[42012],3,1)
+    @test sparse(k) == sparse([2],[1],SpaSM.ZZp{F}[42012],3,1)
 
-    @test sparse(kernel(transpose(sm))) == sparse([1,2,3,4],[1,1,2,2],Spasm.ZZp{F}[2,42012,28010,42012])
+    @test sparse(kernel(transpose(sm))) == sparse([1,2,3,4],[1,1,2,2],SpaSM.ZZp{F}[2,42012,28010,42012])
 end
